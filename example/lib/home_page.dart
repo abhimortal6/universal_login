e0be1f17be2ae85141fa0dcb91e8f9081789d473
Abhi Tripathi <abhijeet.tripathi@joshsoftware.com>
2021-06-28 00:59:40 +0530

    +google signin/logout method

---
+++ example/lib/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:universal_login/universal_login.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  child: Text("Google"),
+                  onPressed: () async {
+                    await UniversalLogin.loginGoogle();
+                  },
+                ),
+                ElevatedButton(
+                  child: Text("Google SignOut"),
+                  onPressed: () async {
+                    await UniversalLogin.logoutGoogle();
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/universal_login.dart
+++ lib/universal_login.dart
@@ -1,14 +1,53 @@
-
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class UniversalLogin {
-  static const MethodChannel _channel =
-      const MethodChannel('universal_login');
+  static const MethodChannel _channel = const MethodChannel('universal_login');
 
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static Future<FirebaseApp> initializeFirebase() async {
+    return await Firebase.initializeApp();
+  }
+
+  static FirebaseAuth getFirebaseAuthInstance() {
+    return FirebaseAuth.instance;
+  }
+
+  static loginGoogle({SignInOption? signInOption}) async {
+    bool alreadySigned = await GoogleSignIn().isSignedIn();
+    if (!alreadySigned) {
+      GoogleSignInAccount? userAccount = await GoogleSignIn(signInOption: signInOption ?? SignInOption.standard).signIn();
+
+      if (userAccount == null) {
+        return;
+      } else {
+        final googleAuth = await userAccount.authentication;
+
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+        UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+        print(userCredential.user!.displayName);
+      }
+    } else {
+      print("Already Signed In: ${FirebaseAuth.instance.currentUser!.displayName}");
+    }
+  }
+
+  static logoutGoogle() async {
+    bool alreadySigned = await GoogleSignIn().isSignedIn();
+    if (alreadySigned) {
+      await GoogleSignIn().signOut();
+      await FirebaseAuth.instance.signOut();
+    } else {}
   }
 }
